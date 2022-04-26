94bdc6eb366b60332269f7a8285aabd05a9cc861
gutkedu <eduardo.pedogutkoski@gmail.com>
2022-04-25 23:27:38 -0300

    feat: Whitelist interface and crypto devs constructor

--- hardhat-js/contracts/CryptoDevs.sol
+++ hardhat-js/contracts/CryptoDevs.sol
@@ -1,1 +1,19 @@
-  // SPDX-License-Identifier: MIT
+// SPDX-Lic // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IWhitelist.sol";
+
+contract CryptoDevs is ERC721Enumerable, Ownable {
+    string _baseTokenURI;
+
+    IWhitelist whitelist;
+
+    constructor(string memory _baseURI, address whitelistContract)
+        ERC721("Crypto Devs", "CD")
+    {
+        _baseTokenURI = _baseURI;
+        whitelist = IWhitelist(whitelistContract);
+    }
+}
