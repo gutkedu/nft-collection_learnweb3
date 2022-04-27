97c5e52021c39d109301f5716de565822dec9f44
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2022-04-27 19:49:09 -0300

    feat: cryptodevs.sol complete

--- hardhat-js/contracts/CryptoDevs.sol
+++ hardhat-js/contracts/CryptoDevs.sol
@@ -13,11 +13,17 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
     uint256 public maxTokenIds = 20;
     uint256 public tokenIds;
     uint256 public _price = 0.01 ether;
+    bool public _paused;
 
-    constructor(string memory _baseURI, address whitelistContract)
+    modifier onlyWhenNotPaused() {
+        require(!_paused, "Contract currently paused");
+        _;
+    }
+
+    constructor(string memory baseURI, address whitelistContract)
         ERC721("Crypto Devs", "CD")
     {
-        _baseTokenURI = _baseURI;
+        _baseTokenURI = baseURI;
         whitelist = IWhitelist(whitelistContract);
     }
 
@@ -26,17 +32,50 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
         presaleEnded = block.timestamp + 5 minutes;
     }
 
-    function presaleMain() public payable {
+    function presaleMint() public payable {
         require(
             presaleStarted && block.timestamp < presaleEnded,
             "Presale ended"
         );
-        require(whitelist.whitelistedAddresses(msg.sender));
+        require(
+            whitelist.whitelistedAddresses(msg.sender),
+            "You are not in the whitelist"
+        );
         require(tokenIds < maxTokenIds, "Exceeded the limit");
         require(msg.value >= _price, "Ether sent is not correct");
 
         tokenIds += 1;
-
         _safeMint(msg.sender, tokenIds);
     }
+
+    function mint() public payable {
+        require(
+            presaleStarted && block.timestamp >= presaleEnded,
+            "Presale has not ended yet"
+        );
+        require(tokenIds < maxTokenIds, "Exceeded the limit");
+        require(msg.value >= _price, "Ether sent is not correct");
+
+        tokenIds += 1;
+        _safeMint(msg.sender, tokenIds);
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function setPaused(bool val) public onlyOwner {
+        _paused = val;
+    }
+
+    function withdraw() public onlyOwner {
+        address _owner = owner();
+        uint256 amount = address(this).balance;
+        (bool sent, ) = _owner.call{value: amount}("");
+        require(sent, "Failed to send ether");
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
 }
