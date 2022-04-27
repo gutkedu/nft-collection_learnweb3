7689d21450adbe0900296c56cc3dbb7d06fad80f
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2022-04-26 23:54:39 -0300

    feat: adding presale functions to crypto dev contract

--- hardhat-js/contracts/CryptoDevs.sol
+++ hardhat-js/contracts/CryptoDevs.sol
@@ -7,13 +7,36 @@ import "./IWhitelist.sol";
 
 contract CryptoDevs is ERC721Enumerable, Ownable {
     string _baseTokenURI;
-
     IWhitelist whitelist;
+    bool public presaleStarted;
+    uint256 public presaleEnded;
+    uint256 public maxTokenIds = 20;
+    uint256 public tokenIds;
+    uint256 public _price = 0.01 ether;
 
     constructor(string memory _baseURI, address whitelistContract)
         ERC721("Crypto Devs", "CD")
     {
         _baseTokenURI = _baseURI;
         whitelist = IWhitelist(whitelistContract);
+    }
+
+    function startPresale() public onlyOwner {
+        presaleStarted = true;
+        presaleEnded = block.timestamp + 5 minutes;
+    }
+
+    function presaleMain() public payable {
+        require(
+            presaleStarted && block.timestamp < presaleEnded,
+            "Presale ended"
+        );
+        require(whitelist.whitelistedAddresses(msg.sender));
+        require(tokenIds < maxTokenIds, "Exceeded the limit");
+        require(msg.value >= _price, "Ether sent is not correct");
+
+        tokenIds += 1;
+
+        _safeMint(msg.sender, tokenIds);
     }
 }
